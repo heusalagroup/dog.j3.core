6312e1dbf4178d88c432e77c7c0737b0ee7ace72
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-03 07:08:46 +0200

    Added missing JSDocs

--- J3Cell.ts
+++ J3Cell.ts
@@ -4,12 +4,32 @@ import { J3CellValueType } from "./J3CellValueType";
 import { J3Formula } from "./J3Formula";
 import { J3Coordinate } from "./J3Coordinate";
 
+/**
+ * A cell in a `J3Grid`.
+ */
 export interface J3Cell {
+    /**
+     * The coordinate of the cell.
+     */
     readonly coordinate : J3Coordinate;
+    /**
+     * The value of the cell. Can be a string, number, or boolean.
+     */
     readonly value      : J3CellValueType;
+    /**
+     * An optional formula for the cell.
+     */
     readonly formula   ?: J3Formula;
 }
 
+/**
+ * Creates a new `J3Cell` object.
+ *
+ * @param {J3Coordinate} coordinate - The coordinate of the cell.
+ * @param {J3CellValueType} value - The value of the cell.
+ * @param {J3Formula} [formula] - An optional formula for the cell.
+ * @returns {J3Cell} A new `J3Cell` object.
+ */
 export function createJ3Cell (
     coordinate : J3Coordinate,
     value      : J3CellValueType,
